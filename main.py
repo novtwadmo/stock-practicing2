28f8dbea1ba6328b3165a6ec554a7b95bf396c8a
novtwadmo <david9106432@gmail.com>
2024-09-16 17:08:28 +0800

    stock-fastapi

--- main.py
+++ main.py
@@ -34,5 +34,4 @@ async def get_stock_info(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "stocks": stock_data})
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
